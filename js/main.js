0d0e91045d3aae8ff7430436795434342d15ec18
Bartosiak <bartosiak.contact@gmail.com>
2023-10-18 14:09:16 +0200

    stworzenie pliku

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let number1 = 5;
+let number2 = 15;
+
+function isItTwenty(num1, num2) {
+    if (num1 === 20 || num2 === 20 || num1 + num2 === 20) {
+        return true;
+    } else {
+        return false;
+    }
+}
+console.log(isItTwenty(number1, number2));
