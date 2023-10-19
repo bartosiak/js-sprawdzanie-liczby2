0809198a8b27c7a3e665c358888423888eb186af
bartosiak <133219378+bartosiak@users.noreply.github.com>
2023-10-19 11:00:29 +0200

    dodanie znaku mniejszości przy porównaniu sumy liczb

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ let number1 = 5;
 let number2 = 15;
 
 function isItTwenty(num1, num2) {
-    if (num1 === 20 || num2 === 20 || num1 + num2 === 20) {
+    if (num1 === 20 || num2 === 20 || num1 + num2 <= 20) {
         return true;
     } else {
         return false;
